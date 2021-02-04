bc0bc48b35539a7882e22ab157df00783d62c4e2
Ons Ghariani <ons@Onss-MacBook-Pro.local>
2021-02-03 18:54:03 -0800

    add output statementwebpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,10 @@ const htmlWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
     mode: 'production',
     entry: './src/client/index.js',
+    output: {
+      libraryTarget: 'var',
+      library: 'Client'
+    },
     module:{
       rules:[
 
